6a67597ff8720da3b3f57ad4b2746135bfff6270
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-09-12 06:36:17 +0000

    feat: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: { applyBaseStyles: false },
+    }),
+    mdx(),
+    react(),
+  ],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
